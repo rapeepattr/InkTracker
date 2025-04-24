bfc5a4e3f98ef0034d12b84e041bcf2e80943ffe
Rapeepat Treerattrakul <rapeepat.ptpk@gmail.com>
2025-04-24 20:39:40 +0700

    Add mock detail log to UI

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -23,55 +23,90 @@ const BookDetail = () => {
     if (!book) return <div className="text-center mt-10">Loading...</div>
     return (
         <div className='w-[80%] mx-auto'>
-            <div className="stats shadow flex justify-center">
-                <div className="stat">
-                    <div className="stat-figure text-primary">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            className="inline-block h-8 w-8 stroke-current">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                        </svg>
-                    </div>
-                    <div className="stat-title">Total Pages</div>
-                    <div className="stat-value text-primary">356</div>
-                </div>
+            <div className='flex gap-3'>
+                <div>
+                    <div className="stats border border-base-300 flex justify-center">
+                        <div className="stat">
+                            <div className="stat-title">Total Pages</div>
+                            <div className="stat-value">356</div>
+                        </div>
 
-                <div className="stat">
-                    <div className="stat-figure text-secondary">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            className="inline-block h-8 w-8 stroke-current">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                        </svg>
-                    </div>
-                    <div className="stat-title">Page Reads</div>
-                    <div className="stat-value text-secondary">132</div>
-                </div>
+                        <div className="stat">
+                            <div className="stat-title">Page Reads</div>
+                            <div className="stat-value">132</div>
+                        </div>
 
-                <div className="stat">
-                    <div className="stat-figure text-secondary">
-                        <div className="avatar online">
-                            <div className="w-16 rounded-full">
-                                <img src={`https://api.dicebear.com/7.x/initials/svg?seed=${book.title.charAt(0)}`} />
-                            </div>
+                        <div className="stat">
+                            <div className="stat-value">46%</div>
+                            <div className="stat-title">Reads done</div>
+                            <div className="stat-desc text-secondary">224 pages remaining</div>
                         </div>
                     </div>
-                    <div className="stat-value">46%</div>
-                    <div className="stat-title">Reads done</div>
-                    <div className="stat-desc text-secondary">224 pages remaining</div>
+
+                    <div class="stats border border-base-300 flex mt-3">
+                        <div class="stat">
+                            <div className="stat-figure text-secondary">
+                                <div className="avatar online">
+                                    <div className="w-16 rounded-full">
+                                        <img src={`https://api.dicebear.com/7.x/initials/svg?seed=${book.title.charAt(0)}`} />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="stat-value">Atomic Habit</div>
+                            <div class="stat-desc">James Clear</div>
+                            <div class="stat-desc mt-[2rem]">A practical guide to building good habits and breaking bad ones.</div>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="stats border border-base-300 w-full">
+                    <div className="stat justify-items-center items-center text-center">
+                        <div className="stat-title text-gray-500 text-sm">Total Pages You've Read</div>
+                        <div className='flex gap-4 justify-center items-center mt-3'>
+                            <button className="btn btn-circle btn-error text-white text-lg btn-sm font-bold">
+                                -
+                            </button>
+                            <div>
+                                <div className="stat-value text-5xl">132</div>
+                            </div>
+                            <button className="btn btn-circle btn-success text-white text-lg btn-sm font-bold">
+                                +
+                            </button>
+                        </div>
+
+                        <progress className="progress progress-warning w-56" value="70" max="100"></progress>
+                        <button className="btn btn-soft btn-warning mt-4 font-semibold">
+                            Save My Progress
+                        </button>
+
+                        <div className="text-xs text-gray-400 mt-2">
+                            Update the number of pages you've read so far 📘
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="overflow-x-auto mt-8">
+                <table className="table table-xs">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Action</th>
+                            <th>Pages read</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>24 Apr 2025</td>
+                            <td>12:10 AM</td>
+                            <td>Add reads</td>
+                            <td>16</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
