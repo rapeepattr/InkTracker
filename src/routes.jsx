b85e6dcbcaf1eca4901bd26ab581201be55ed0e3
Rapeepat Treerattrakul <rapeepat.ptpk@gmail.com>
2025-05-07 23:53:27 +0700

    Setting default path

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { AuthContext } from './contexts/authContext'
 import Home from './pages/Home'
 import AddBook from './pages/AddBook'
 import EditBook from './pages/EditBook'
@@ -10,16 +11,19 @@ import Register from './pages/Register'
 import Ranking from './pages/Ranking'
 
 const AppRoutes = () => {
+    const { user } = useContext(AuthContext)
+
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/add" element={<AddBook />} />
+            <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+            <Route path="/add" element={user ? <AddBook /> : <Navigate to="/login" />} />
+            <Route path="/edit/:id" element={user ? <EditBook /> : <Navigate to="/login" />} />
+            <Route path="/books" element={user ? <BookList /> : <Navigate to="/login" />} />
+            <Route path="/books/:id" element={user ? <BookDetail /> : <Navigate to="/login" />} />
+            <Route path="/ranking" element={user ? <Ranking /> : <Navigate to="/login" />} />
+
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/ranking" element={<Ranking />} />
-            <Route path="/edit/:id" element={<EditBook />} />
-            <Route path='/books' element={<BookList />} />
-            <Route path="/books/:id" element={<BookDetail />}></Route>
         </Routes>
     )
 }
